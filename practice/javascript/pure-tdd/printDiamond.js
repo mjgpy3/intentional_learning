e6b467ee2f78a3e7ce0949d3da834f104fd0bc90
Michael Gilliland <mjg.py3@gmail.com>
2014-11-15 17:06:24 -0500

    Green 4

--- practice/javascript/pure-tdd/printDiamond.js
+++ practice/javascript/pure-tdd/printDiamond.js
@@ -1,4 +1,8 @@
 var printDiamond = function (letter) {
+    if (letter === 'C') {
+        return '  A\n B B \nC   C\n B B \n  A';
+    }
+
     if (letter === 'B') {
         return ' A\nB B\n A';
     }
