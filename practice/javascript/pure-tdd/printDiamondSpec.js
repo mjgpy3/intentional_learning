22749a3f779bc7cb03c3b63a13c344ad67d84421
Michael Gilliland <mjg.py3@gmail.com>
2014-11-15 16:56:49 -0500

    Red 3

--- practice/javascript/pure-tdd/printDiamondSpec.js
+++ practice/javascript/pure-tdd/printDiamondSpec.js
@@ -7,4 +7,9 @@ describe('printDiamond', function () {
             expect(printDiamond('A')).toEqual('A');
         });
     });
+    describe('given the letter B', function () {
+        it('returns the A-B-A diamond', function () {
+            expect(printDiamond('B')).toEqual(' A\nB B\n A');
+        });
+    });
 });
