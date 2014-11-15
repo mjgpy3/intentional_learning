cabde0c129d1c5ee33da82cad9136b3bfa4c93a0
Michael Gilliland <mjg.py3@gmail.com>
2014-11-15 18:47:53 -0500

    Final tidying up

--- practice/javascript/pure-tdd/printDiamond.js
+++ practice/javascript/pure-tdd/printDiamond.js
@@ -1,23 +1,21 @@
 var printDiamond = function (letter) {
-    var internal = function (letter) {
-        return letter === 'A' ? 'A' : letter + spacePad(distance('A', letter)*2-1) + letter;
-    };
-    var row = function (to) {
-        return spacePad(distance(to, letter)) +
-            internal(to) +
-            spacePad(distance(to, letter));
-    };
-    var rowsUntil = function () {
-        var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-            i,
-            results = [];
-        for (var i = 0; distance(alpha[i], letter) > 0; i += 1) {
-            results.push(row(alpha[i]));
-        }
-        return results;
-    };
+    var withInternalSpaces = function (l) {
+        return l === 'A'
+            ? 'A'
+            : [l, spacePad(distance('A', l)*2-1), l].join('');
+        },
+        row = function (to) {
+            return [spacePad(distance(to, letter)), withInternalSpaces(to), spacePad(distance(to, letter))].join('');
+        },
+        rowsUntil = function () {
+            var alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', i, results = [];
+            for (var i = 0; distance(alpha[i], letter) > 0; i += 1) {
+                results.push(row(alpha[i]));
+            }
+            return results;
+        };
 
-    return rowsUntil().concat(internal(letter), rowsUntil().reverse()).join('\n');
+    return rowsUntil().concat(withInternalSpaces(letter), rowsUntil().reverse()).join('\n');
 };
 
 var spacePad = function (length) {
