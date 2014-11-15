01a01ead2d1974b5162c1294e98e0994c501a621
Michael Gilliland <mjg.py3@gmail.com>
2014-11-15 16:54:14 -0500

    Red 2

--- practice/javascript/pure-tdd/printDiamondSpec.js
+++ practice/javascript/pure-tdd/printDiamondSpec.js
@@ -2,4 +2,9 @@ describe('printDiamond', function () {
     it('exists', function () {
         expect(printDiamond).toBeDefined();
     });
+    describe('given the letter A', function () {
+        it('returns "A"', function () {
+            expect(printDiamond('A')).toEqual('A');
+        });
+    });
 });
