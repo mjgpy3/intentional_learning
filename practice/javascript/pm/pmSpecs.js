953be877bd7fad4dd9258c5a52bf4a5e931d180c
Michael Gilliland <mjg.py3@gmail.com>
2016-08-07 10:04:01 -0400

    Remove dead git module. commit old change sto specs that I don't remember but pass.

--- practice/javascript/pm/pmSpecs.js
+++ practice/javascript/pm/pmSpecs.js
@@ -1,3 +1,78 @@
+var matching = function (toMatch) {
+    var cases, matcher, matchedResultOr, isMatchingAnObject, addCaseAndGetMatcher;
+
+    cases = [];
+
+    isMatchingAnObject = function () {
+        return typeof toMatch === 'object' && toMatch !== null;
+    };
+
+    addCaseAndGetMatcher = function (p, resultCalculator) {
+        cases.push({
+            isMatch: p,
+            result: resultCalculator
+        });
+
+        return matcher;
+    };
+
+    matchedResultOr = function (resultCalculator) {
+        return function () {
+            matchingCase = cases.find(function (aCase) {
+                return aCase.isMatch(toMatch);
+            });
+
+            if (matchingCase) {
+                return matchingCase.result(toMatch);
+            }
+
+            return resultCalculator(toMatch);
+        };
+    };
+
+    matcher = {
+        match: matchedResultOr(function () {
+            throw new Error('Non-exhaustive patterns when attempting to match ' + toMatch);
+        }),
+        otherwise: function (resultCalculator) {
+            return matchedResultOr(resultCalculator)();
+        },
+        on: {
+            objectWithProperty: function (prop, resultCalculator) {
+                return addCaseAndGetMatcher(function () {
+                    return isMatchingAnObject() && toMatch[prop] !== undefined;
+                },
+                    resultCalculator
+                );
+            },
+            objectWithProperties: function () {
+                var args = [].slice.call(arguments);
+
+                return addCaseAndGetMatcher(function () {
+                    return isMatchingAnObject() && args.slice(0, -1).every(function (prop) {
+                        return toMatch.hasOwnProperty(prop);
+                    });
+                },
+                    args[args.length - 1]
+                );
+            },
+            value: function (exactMatch, resultCalculator) {
+                return addCaseAndGetMatcher(function () {
+                    return toMatch === exactMatch;
+                },
+                    resultCalculator
+                );
+            },
+            object: function (resultCalculator) {
+                return addCaseAndGetMatcher(isMatchingAnObject, resultCalculator);
+            },
+            satisfying: addCaseAndGetMatcher
+        }
+    };
+
+    return matcher;
+};
+
 describe('matching({ foo: "bar", spam: "eggs"}).', function () {
     var described;
 
