1b8bc1c7a0ec6ccfb455a6feaab2c49f35cc2f3c
Michael Gilliland <mjg.py3@gmail.com>
2016-08-12 21:31:50 -0400

    Got a decently interesting example working with prefix ops

--- practice/fsharp/type-providers/EbnfParser.fsx
+++ practice/fsharp/type-providers/EbnfParser.fsx
@@ -21,33 +21,36 @@ module EbnfParser =
 
   let prependChar (c, text) = c.ToString() + text
 
-  let symbol : Parser<char, unit> = anyOf ['['; ']'; '{'; '}'; '('; ')'; '<'; '>'; '\''; '"'; '='; '|'; '.'; ','; ';'; '-']
+  let symbol extra = anyOf <| extra::['['; ']'; '{'; '}'; '('; ')'; '<'; '>'; '='; '|'; '.'; ','; ';'; '-']
 
-  let character = letter <|> digit <|> symbol <|> pchar '_'
+  let character extra = letter <|> digit <|> symbol extra <|> pchar '_'
 
   let identifier : Parser<Ebnf, unit> =
     letter .>>. manyChars (letter <|> digit <|> pchar '_') |>> (Identifier << prependChar)
 
   let terminal =
-        (between (pchar '\'') (pchar '\'') (many1Chars character))
-    <|> (between (pchar  '"') (pchar  '"') (many1Chars character)) |>> Terminal
+        (between (pchar '\'') (pchar '\'') (many1Chars <| character '"'))
+    <|> (between (pchar  '"') (pchar  '"') (many1Chars <| character '\'')) |>> Terminal
 
   let lhs = identifier
 
-  let rec rhs : Parser<Ebnf, unit> = parse.Delay(fun () ->
-        identifier
-    <|> terminal
-    <|> (pchar '[' >>. rhs .>> pchar ']' |>> Optional)
-    <|> (pchar '{' >>. rhs .>> pchar '}' |>> Repetition)
-    <|> (pchar '(' >>. rhs .>> pchar ')' |>> Grouping)
-    <|> (rhs .>> pchar '|' .>>. rhs |>> Alteration)
-    <|> (rhs .>> pchar ',' .>>. rhs |>> Concatination)
-  )
+  let rec rhs : Parser<Ebnf, unit> =
+    let recur = parse.Delay(fun () -> rhs)
+
+    choice [
+      pchar '|' >>.recur .>>. recur |>> Alteration;
+      pchar ',' >>. recur .>>. recur |>> Concatination;
+      identifier;
+      terminal;
+      pchar '[' >>. recur .>> pchar ']' |>> Optional;
+      pchar '{' >>. recur .>> pchar '}' |>> Repetition;
+      pchar '(' >>. recur .>> pchar ')' |>> Grouping
+    ]
 
   let rule : Parser<Rule, unit> = lhs .>> pchar '=' .>>. rhs .>> pchar ';'
 
   let grammar : Parser<Grammar, unit> = sepEndBy rule (pchar ';')
 
-  let r1 = run grammar "integer=\"0\"|[\"-\"],natural_number;"
+  let r1 = run grammar "integer=,|\"0\"[\"-\"]natural_number;"
 
   printfn "%A" r1
